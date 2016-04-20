6a03fd8dbe637243273db1fa892f34e41cae56b9
AndrewHatswell <andrewhatswell@gmail.com>
2016-04-21 00:45:28 +0100

    Added while loop to X & +

--- Cross.py
+++ Cross.py
@@ -8,9 +8,10 @@ from check import *
 
 def cross_piece(last_tile, tile):
     x = 1
-    if tile == (last_tile-(x*3)) or tile == (last_tile+(x*3)) or tile == (last_tile-(x*5)) or tile == (last_tile+(x*5)):
-        return True
-    elif x <= 3:
-        x += 1
-    else:
-        return False
+    while x <= 3:
+        if tile == (last_tile-(x*3)) or tile == (last_tile+(x*3)) or tile == (last_tile-(x*5)) or tile == (last_tile+(x*5)):
+            return True
+        elif x <= 3:
+            x += 1
+        else:
+            return False
