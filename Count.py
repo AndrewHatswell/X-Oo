8f872d557dfe2cccc1537051b0cdf4a3d22d4aa1
Kris Ward <kristopher.ward@tessella.com>
2016-04-23 16:45:37 +0100

    Half way through plus value. Not done: Ad or NonAd

--- Count.py
+++ Count.py
@@ -1,37 +1,37 @@
-class pieces:
+class number_of_pieces:
     cross_count = 2
     plus_count = 2
     adjacent_count = 2
     nonAdjacent_count = 2
 
 
-def piece_count(last_piece):
+def piece_count(last_piece, number_of_pieces):
     if last_piece == 1:
-        pieces.cross_count -= 1
-        return pieces.cross_count
+        number_of_pieces.cross_count -= 1
+        return number_of_pieces.cross_count
     elif last_piece == 2:
-        pieces.plus_count -= 1
-        return pieces.plus_count
+        number_of_pieces.plus_count -= 1
+        return number_of_pieces.plus_count
     elif last_piece == 3:
-        pieces.adjacent_count -= 1
-        return pieces.adjacent_count
+        number_of_pieces.adjacent_count -= 1
+        return number_of_pieces.adjacent_count
     elif last_piece == 4:
-        pieces.nonAdjacent_count -= 1
-        return pieces.nonAdjacent_count
+        number_of_pieces.nonAdjacent_count -= 1
+        return number_of_pieces.nonAdjacent_count
     else:
         return
 
 
 
-#if pieces.cross_count > 0:
+#if number_of_pieces.cross_count > 0:
 #else:
 #print "You have no X's left!"
-#if pieces.plus_count == 0:
+#if number_of_pieces.plus_count == 0:
 #print "You have no +'s left!"
 #else:
-#if pieces.adjacent_count == 0:
+#if number_of_pieces.adjacent_count == 0:
 #print "You have no o's left!"
 #else:
-#if pieces.nonAdjacent_count == 0:
+#if number_of_pieces.nonAdjacent_count == 0:
 #print "You have no O's left!"
 #else:
--- Plus.py
+++ Plus.py
@@ -7,3 +7,16 @@ def plus_piece(last_tile, tile):
             x += 1
         else:
             return False
+
+def plus_piece(CurrentTile, LastTile):
+    z = 1
+    while z <= 3:
+        if (CurrentTile.x == LastTile.x-z)\
+                or (CurrentTile.x == LastTile.x+z)\
+                or (CurrentTile.y == LastTile.y-z)\
+                or (CurrentTile.y == LastTile.y+z):
+            return True
+        elif z <= 3:
+            z += 1
+        else:
+            return False
