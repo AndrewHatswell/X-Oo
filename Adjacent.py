02abf4a9e18689a4ec86c1ca34195a49b58b9f72
AndrewHatswell <andrewhatswell@gmail.com>
2016-04-24 19:51:55 +0100

    All pieces working with co-ords

--- Adjacent.py
+++ Adjacent.py
@@ -1,5 +1,7 @@
 def adjacent_piece(CurrentTile, LastTile):
-    if abs(CurrentTile.x) == abs(LastTile.x - 1) and abs(CurrentTile.y) == abs(LastTile.y - 1):
+    if CurrentTile.x == (LastTile.x - 1) and ((LastTile.y - 1) <= CurrentTile.y <= (LastTile.y + 1)) or \
+            CurrentTile.x == (LastTile.x + 1) and ((LastTile.y - 1) <= CurrentTile.y <= (LastTile.y + 1)) or \
+            CurrentTile.x == LastTile.x and ((LastTile.y - 1) <= CurrentTile.y <= (LastTile.y + 1)):
         return True
     else:
         return False
--- Nonadjacent.py
+++ Nonadjacent.py
@@ -1,15 +1,7 @@
 def nonadjacent_piece(CurrentTile, LastTile):
-    if abs(CurrentTile.x) == abs(LastTile.x - 1) and abs(CurrentTile.y) == abs(LastTile.y - 1):
+    if CurrentTile.x == (LastTile.x - 1) and ((LastTile.y - 1) <= CurrentTile.y <= (LastTile.y + 1)) or \
+            CurrentTile.x == (LastTile.x + 1) and ((LastTile.y - 1) <= CurrentTile.y <= (LastTile.y + 1)) or \
+            CurrentTile.x == LastTile.x and ((LastTile.y - 1) <= CurrentTile.y <= (LastTile.y + 1)):
         return False
     else:
         return True
-
-
-
-
-
-#    if tile == (last_tile - 5) or tile == (last_tile - 4) or tile == (last_tile -3) or tile == (last_tile - 1) or \
-#            tile == (last_tile + 1) or tile == (last_tile + 3) or tile == (last_tile + 4) or tile == (last_tile +5):
-#        return False
-#    else:
-#        return True
